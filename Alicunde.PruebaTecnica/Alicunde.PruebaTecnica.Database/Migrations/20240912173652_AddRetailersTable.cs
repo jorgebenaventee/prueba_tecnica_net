dedbe0fd138e7704d81c829181b0057297d44343
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-09-12 19:54:51 +0200

    Add database migrations

---
+++ Alicunde.PruebaTecnica/Alicunde.PruebaTecnica.Database/Migrations/20240912173652_AddRetailersTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alicunde.PruebaTecnica.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRetailersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "retailers",
+                columns: table => new
+                {
+                    ReCode = table.Column<string>(type: "text", nullable: false),
+                    ReName = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    CodingScheme = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_retailers", x => x.ReCode);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "retailers");
+        }
+    }
+}
